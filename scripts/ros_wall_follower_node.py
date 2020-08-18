d1e6d49b4692e46702c2c9841f10ebd376f16a5b
Fernando Zaragoza <fer.zm@outlook.com>
2020-08-18 21:03:36 +0000

    Adding 3 sides control (right, left, center). Adjust in ranges.

--- scripts/ros_wall_follower_node.py
+++ scripts/ros_wall_follower_node.py
@@ -28,8 +28,8 @@ class WallFollower(object):
         self.error = [0.0, 0.0]
         self.u = 0.0
         self.steering_output = 0.0
-        self.pa_donde=''
         self.vel = 2.0
+        self.control_side = "RIGHT" # "LEFT" or "CENTER"
 
         rospy.init_node("ros_wall_follower_node")
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.laserCallback, queue_size=1)
@@ -43,16 +43,12 @@ class WallFollower(object):
             
     def laserCallback(self, msg):
         self.regions = {
-            'DER'   : min(min(msg.ranges[230:250]), 30),
-            'F_DER'  : min(min(msg.ranges[251:420]), 30),
+            'DER'   : min(min(msg.ranges[107:322]), 30),
+            'F_DER'  : min(min(msg.ranges[323:420]), 30),
             'FRONT': min(min(msg.ranges[421:660]), 30),
-            'F_IZQ'  : min(min(msg.ranges[661:784]), 30),
-            'IZQ'   : min(min(msg.ranges[785:1079]), 30),
+            'F_IZQ'  : min(min(msg.ranges[661:751]), 30),
+            'IZQ'   : min(min(msg.ranges[752:967]), 30),
         }
-        #rospy.loginfo(regions)
-        # self.angle_increment = msg.angle_increment
-        # self.steps = (msg.angle_max - msg.angle_min) / self.angle_increment
-        # rospy.loginfo("Step is {}, Distances per region {}".format(self.steps, self.regions['Der']))
 
     def setCarMovement(self, steering_angle, steering_angle_velocity, speed,
                         acceleration, jerk):
@@ -68,19 +64,24 @@ class WallFollower(object):
             self.vel= -5.0
             signo = -1 if (self.steering_output >= 0) else 1
             self.steering_output = signo
-        elif self.regions['FRONT']< 0.9 and self.regions['FRONT']> 0.2: 
+        elif  self.regions['FRONT'] > 0.2 and self.regions['FRONT'] < 0.9: 
             signo = -1 if (self.steering_output >= 0) else 1
             self.steering_output = signo if self.vel == -5.0 else self.steering_output * 1
-        elif self.regions['FRONT']< 1.4 and self.regions['FRONT']> 0.9:
+        elif self.regions['FRONT'] > 0.9 and self.regions['FRONT'] < 1.4:
             self.vel = 1.0
         elif self.regions['FRONT'] > 1.4:
             self.vel= 2.0
-        self.setCarMovement(self.steering_output, 0.08,self.vel, 0.0, 0.0)
-        # rospy.loginfo(self.pa_donde)
-        #self.pub_drive.publish(self.acker_msg)
+        self.setCarMovement(self.steering_output, 0.08, self.vel, 0.0, 0.0)
+        # self.pub_drive.publish(self.acker_msg)
 
     def calcControl(self):
-        self.error[0] = self.setpoint - self.regions['DER']
+        if (self.control_side == "RIGHT"):
+            self.error[0] = self.setpoint - self.regions['DER']
+        elif (self.control_side == "LEFT"):
+            self.error[0] = self.regions['IZQ'] - self.setpoint
+        else:
+            self.setpoint = (self.regions['DER'] + self.regions['IZQ'])/2
+            self.error[0] = self.setpoint - self.regions['DER']
         Up = self.gains['Kp'] * self.error[0]
         Ui = self.gains['Ki'] * (self.error[0] - self.error[1]) / 2
         Ud = self.gains['Kd'] * (1 / self.dt) * (self.error[0] - self.error[1])
@@ -96,20 +97,27 @@ class WallFollower(object):
 
     def timerCallback(self, event):
         self.takeAction()
-        #rospy.loginfo("x:{}, y:{}, ".format(self.x,self.y))
-        rospy.loginfo("waypoints:{},{} ".format(self.waypoints_x,self.waypoints_y))
-        rospy.loginfo("{}, {}, {}, {},".format(self.acker_msg.drive.steering_angle, self.acker_msg.drive.speed, self.regions['IZQ'],self.regions['FRONT']))
-        self.wpdict ={"x":self.waypoints_x,"y":self.waypoints_y}
         self.pub_drive.publish(self.acker_msg)
-        self.segs = self.segs+1
+        
+        self.wpdict ={"x": self.waypoints_x,"y": self.waypoints_y}
+        self.segs = self.segs + 1
         if self.segs == 20:
             self.waypoints_x.append(self.x)
             self.waypoints_y.append(self.y)
             self.segs=0
+        
         DataOutput = pd.DataFrame(self.wpdict)
         DataOutput.to_csv("ros_wall_follower/scripts/coords.csv" , index=False)
 
+        # rospy.loginfo("waypoints:{},{} ".format(self.waypoints_x,self.waypoints_y))
+        # rospy.loginfo("{}, {}, {}, {},".format(self.acker_msg.drive.steering_angle, self.acker_msg.drive.speed, self.regions['IZQ'],self.regions['FRONT']))
 
+        if (self.control_side == "RIGHT"):
+            rospy.loginfo("DER: {}, Giro: {}, Error: {}".format(self.regions['DER'], self.acker_msg.drive.steering_angle, self.error[0]))
+        elif (self.control_side == "LEFT"):
+            rospy.loginfo("IZQ: {}, Giro: {}, Error: {}".format(self.regions['IZQ'], self.acker_msg.drive.steering_angle, self.error[0]))
+        else:
+            rospy.loginfo("DER: {}, IZQ:{}, Giro: {}, Error: {}".format(self.regions['DER'], self.regions['IZQ'], self.acker_msg.drive.steering_angle, self.error[0]))
 
 
 if __name__ == "__main__":
