d08d5c21e6b134ee1cf6e89f122b5faa647615e2
DESKTOP-D1FOK76\Chets <TheKillercheto@gmail.com>
2020-08-20 20:29:32 -0500

    creación lateral kinematics

---
+++ scripts/ros_lateral_kinematics_node.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+from nav_msgs.msg import Odometry
+from collections import defaultdict
+from tf import transformations
+import math
+import pandas as pd
+
+df=pd.read_csv("coords100ms.csv"sep=',')
+class WallFollower(object):
+    def __init__(self):
+        self.acker_msg = AckermannDriveStamped()
+        self.regions = defaultdict(lambda:float)
+        self.gains = {
+            'Kp': 1.1,#1.0,
+            'Ki': 0.0025,#0.0025,
+            'Kd': 0.0005,#0.0005,
+        }
+        self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.laserCallback, queue_size=1)
+        self.max_steering = 1
+        self.setpoint = 0.0
+        self.dt = 0.01
+        self.error = [0.0, 0.0]
+        self.u = 0.0
+        self.steering_output = 0.0
+        self.vel = 2.0
+
+        rospy.init_node("ros_lateral_kinematics_node")
+        self.pub_drive = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
+        self.odom_= rospy.Subscriber("/odom", Odometry,self.odomCallback,queue_size=1)
+        rospy.Timer(rospy.Duration(self.dt), self.timerCallback)
+    
+    def odomCallback(self, msg):
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        quaternion = (
+            msg.pose.pose.orientation.x,
+            msg.pose.pose.orientation.y,
+            msg.pose.pose.orientation.z,
+            msg.pose.pose.orientation.w)
+        euler = transformations.euler_from_quaternion(quaternion)
+        self.yaw_ = euler[2]
+
+    def laserCallback(self, msg):
+        self.regions = {
+            'DER'   : min(min(msg.ranges[138:539]), 30),
+            'IZQ'   : min(min(msg.ranges[541:940]), 30),
+        }
+        self.area=0.0
+        self.moment=0.0
+        self.centroid=0.0
+        for i in range(140,940):
+            self.area = self.area+(msg.ranges[i]*.25)
+            self.moment = self.moment+(i*msg.ranges[i]*.25)
+        self.centroid=self.moment/self.area
+
+
+    def setCarMovement(self, steering_angle, steering_angle_velocity, speed,
+                        acceleration, jerk):
+        self.acker_msg.drive.speed = speed
+        # self.acker_msg.drive.acceleration = acceleration
+        self.acker_msg.drive.steering_angle_velocity= steering_angle_velocity
+        self.acker_msg.drive.steering_angle = steering_angle
+        # self.acker_msg.drive.jerk = jerk
+    
+    def takeAction(self):
+        self.calcControl()
+        self.setCarMovement(self.steering_output, 0.08, self.vel, 0.0, 0.0)
+
+    def calcControl(self):
+        self.setpoint = 540 
+        self.error[0] = self.centroid-self.setpoint 
+        Up = self.gains['Kp'] * self.error[0]
+        Ui = self.gains['Ki']*self.dt * (self.error[0] - self.error[1]) / 2
+        Ud = self.gains['Kd'] * (1 / self.dt) * (self.error[0] - self.error[1])
+
+        U = Up + Ui + Ud
+
+        self.steering_output = ((self.max_steering) / (self.gains['Kp'] * 270) * U)
+
+        sign = 1 if (self.steering_output >= 0) else -1
+        self.steering_output = (sign * self.max_steering) if (abs(self.steering_output) >= self.max_steering) else self.steering_output
+
+        self.error[1] = self.error[0]
+
+    def timerCallback(self, event):
+        self.takeAction()
+        self.pub_drive.publish(self.acker_msg)
+        self.wpdict ={"x": self.waypoints_x,"y": self.waypoints_y}
+        self.segs = self.segs + 1
+        if self.segs == 20.0:
+            self.waypoints_x.append(self.x)
+            self.waypoints_y.append(self.y)
+            self.segs=0
+        DataOutput = pd.DataFrame(self.wpdict)
+        DataOutput.to_csv("ros_wall_follower/scripts/coords.csv" , index=False)
+       
+
+
+if __name__ == "__main__":
+    robot = WallFollower()
+    # robot.main()
+    rospy.spin()
