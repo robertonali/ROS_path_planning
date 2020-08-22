14f5ca8673bc34fc97ad7c48c1b1d12fd43b5daa
Fernando Zaragoza <fer.zm@outlook.com>
2020-08-22 21:45:04 +0000

    Cleanup file ros_wf.py with pandas. Working.

--- scripts/ros_wf.py
+++ scripts/ros_wf.py
@@ -1,70 +1,76 @@
 #! /usr/bin/env python
 
 import rospy
+import numpy as np
+import pandas as pd
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 from nav_msgs.msg import Odometry
 from collections import defaultdict
 from tf import transformations
-import math
-import pandas as pd
-import numpy as np
 
-class ePID_M(object):
+class PID(object):
     def __init__(self, kp, ki, kd):
         self.dt       = 0.01
         self.setpoint = 0
         self.gains    = {'Kp': kp, 'Ki': ki, 'Kd': kd}
         self.error    = [0.0, 0.0]
         self.U        = 0.0
-        self.steering_output = 0.0
     
     def calculateControl(self, centroid):
         self.setpoint = 0
         self.error[0] = centroid - self.setpoint #self.regions['IZQ']- self.regions['DER']
         Up = self.gains['Kp'] * self.error[0]
-        Ui = self.gains['Ki'] * ((self.error[0] - self.error[1]) / 2) * self.dt
-        Ud = self.gains['Kd'] * (self.error[0] - self.error[1])/ self.dt
+        Ui = self.gains['Ki'] * ((self.error[0] + self.error[1]) / 2) * self.dt
+        Ud = self.gains['Kd'] * (self.error[0] - self.error[1]) * (1 / self.dt)
 
         self.U = Up + Ui + Ud
 
-        #self.steering_output = ((self.max_steering) / (self.gains['Kp'] * 270) * U)
-        self.steering_output = min(max(-1., self.U),1)
-        rospy.loginfo("Control: {}".format(self.U))
+        # self.steering_output = ((self.max_steering) / (self.gains['Kp'] * 270) * U)
         # sign = 1 if (self.steering_output >= 0) else -1
         # self.steering_output = (sign * self.max_steering) if (abs(self.steering_output) >= self.max_steering) else self.steering_output
-
+        # self.steering_output = min(max(-1., self.U),1)
         self.error[1] = self.error[0]
+        return min(max(-1.0, self.U), 1.0)
 
 class Odom(object):
     def __init__(self):
         self.waypoints_x = list()
         self.waypoints_y = list()
+        self.wpdict      = {'x': list(), 'y': list()}
         self.current_pos = {'x': 0.0, 'y': 0.0}
+        self.prev_pos    = {'x': 0.0, 'y': 0.0}
         self.current_vel = {'x': 0.0, 'y': 0.0,
                 'total': 0.0}
+        self.track       = 0.28
+        self.wheelbase   = 0.40
 
 class Centroid(object):
     def __init__(self):
         self.centroid              = 0.0
+        # self.moment                = 0.0
+        # self.area                  = 0.0
         self.normalized_centroid   = 0.0
 
-class WallFollower(ePID_M, Odom, Centroid):
+class WallFollower(PID, Odom, Centroid):
     def __init__(self):
         # self.pid = PID(1.0, 0.0, 0.0)
-        ePID_M.__init__(self, 3, 0.0, 0.01)
+        PID.__init__(self, 2.12, 0.00, 0.42)  # 1.50, 0.4
+        # 1.70, 0.4
+        # 2.12, 0.42
 
         Centroid.__init__(self)
 
         Odom.__init__(self)
 
         self.max_steering    = 1.0
-        self.vel             = 4.0
+        self.steering_output = 0.0
+        self.vel             = 3.0
         self.acker_msg       = AckermannDriveStamped()
-        self.regions         = defaultdict(lambda:float)
+        self.regions         = defaultdict(lambda: float)
 
-        self.segs = 0.0
-        
+        rospy.init_node("dummy_agent")
+
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
         self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.laserCallback, queue_size=1)
         self.odom_sub  = rospy.Subscriber("/odom", Odometry, self.odomCallback, queue_size=1)
@@ -86,44 +92,27 @@ class WallFollower(ePID_M, Odom, Centroid):
         #     'DER'   : np.array(msg.ranges[138:539]),
         #     'IZQ'   : np.array(msg.ranges[541:940])
         # }
+        # for i in range(140,940):
+        #     self.area = self.area + (msg.ranges[i] * .25)
+        #     self.moment = self.moment + (i * msg.ranges[i] * .25)
+        # self.centroid = self.moment / self.area
 
-
-        # self.centroid = np.divide(np.sum(np.multiply(msg.ranges, np.arange(1080))),
-        #                     np.sum(msg.ranges))
-
-        self.regions = {
-            'DER'   : min(min(msg.ranges[138:539]), 30),
-            # 'F_DER'  : min(min(msg.ranges[323:420]), 30),
-            # 'FRONT': min(min(msg.ranges[421:659]), 30),
-            # 'F_IZQ'  : min(min(msg.ranges[661:751]), 30),
-            'IZQ'   : min(min(msg.ranges[541:940]), 30),
-        }
-        self.area=0.0
-        self.moment=0.0
-        self.centroid=0.0
-        for i in range(140,940):
-            self.area = self.area+(msg.ranges[i]*.25)
-            self.moment = self.moment+(i*msg.ranges[i]*.25)
-        self.centroid=self.moment/self.area
-        self.normalized_centroid = (self.centroid / 540) -1
-        #rospy.loginfo(self.centroid)
+        self.centroid = np.divide(np.sum(np.multiply(msg.ranges[140:940], np.arange(140, 940))),
+                    np.sum(msg.ranges[140:940]))
+        self.normalized_centroid = ((self.centroid / 400) - 1.35)
+        rospy.loginfo(self.normalized_centroid)
 
 
     def setCarMovement(self, steering_angle, steering_angle_velocity, speed,
                         acceleration, jerk):
-        self.acker_msg = AckermannDriveStamped()
         self.acker_msg.drive.steering_angle = steering_angle
         # self.acker_msg.drive.steering_angle_velocity= steering_angle_velocity
         self.acker_msg.drive.speed = speed
         # self.acker_msg.drive.acceleration = acceleration
         # self.acker_msg.drive.jerk = jerk
-
-
-        self.drive_pub.publish(self.acker_msg)
     
     def takeAction(self):
-        pass
-        #self.calcControl()
+        self.calcControl()
         # if self.regions['FRONT'] <= 0.2:
         #     self.vel= -3.0
         #     signo = -1 if (self.steering_output >= 0) else 1
@@ -162,19 +151,24 @@ class WallFollower(ePID_M, Odom, Centroid):
         # # self.steering_output = (sign * self.max_steering) if (abs(self.steering_output) >= self.max_steering) else self.steering_output
 
         # self.error[1] = self.error[0]
-    def timerCallback(self, elapsed):
+    def timerCallback(self, event):
         # self.takeAction()
-        self.calculateControl(self.normalized_centroid)
+        self.steering_output = self.calculateControl(self.normalized_centroid)
+        self.vel = 5.0 - abs(self.steering_output) * 2.4
         self.setCarMovement(self.steering_output, 0.0, self.vel, 0.0, 0.0)
-        self.wpdict ={"x": self.waypoints_x,"y": self.waypoints_y}
-        self.segs = self.segs + 1
-        if self.segs == 30.0:
-            self.waypoints_x.append(self.current_pos['x'])
-            self.waypoints_y.append(self.current_pos['y'])
-            self.segs=0
-        DataOutput = pd.DataFrame(self.wpdict)
-        DataOutput.to_csv("ros_wall_follower/scripts/coords_mamado.csv" , index=False)
-   
+        self.drive_pub.publish(self.acker_msg)
+        self.getWaypoints()
+        
+        # self.wpdict ={"x": self.waypoints_x,"y": self.waypoints_y}
+        # self.segs = self.segs + 1
+
+        # DataOutput = pd.DataFrame(self.wpdict)
+        # DataOutput.to_csv("coords.csv" , index=False)
+        # rospy.loginfo(self.z1)
+        #rospy.loginfo(self.z2)
+        # rospy.loginfo("waypoints:{},{} ".format(self.waypoints_x,self.waypoints_y))
+        # rospy.loginfo("{}, {}, {}, {},".format(self.acker_msg.drive.steering_angle, self.acker_msg.drive.speed, self.regions['IZQ'],self.regions['FRONT']))
+
         # if (self.control_side == "RIGHT"):
         #     rospy.loginfo("DER: {}, Giro: {}, Error: {}".format(self.regions['DER'], self.acker_msg.drive.steering_angle, self.error[0]))
         # elif (self.control_side == "LEFT"):
@@ -182,9 +176,20 @@ class WallFollower(ePID_M, Odom, Centroid):
         # else:
         #     rospy.loginfo("DER: {}, IZQ:{}, Giro: {}, Error: {}".format(self.regions['DER'], self.regions['IZQ'], self.acker_msg.drive.steering_angle, self.error[0]))
 
+        rospy.loginfo("Steer: {}, Error:{} vel:{}, Centroid: {}".format(self.steering_output,
+                        self.error[0], self.current_vel['total'], self.centroid))
+
+    def getWaypoints(self):
+        if (np.hypot((self.current_pos['x'] - self.prev_pos['x']), (self.current_pos['y'] - self.prev_pos['y']))) >= 0.2:
+            self.waypoints_x.append(self.current_pos['x'])             # list appending x data
+            self.waypoints_y.append(self.current_pos['y'])             # list appending y data
+            self.prev_pos['x'] = self.current_pos['x']                 # change value of prev x on dict
+            self.prev_pos['y'] = self.current_pos['y']                 # change value of prev y on dict
+            self.wpdict ={"x": self.waypoints_x,"y": self.waypoints_y} # dictionary of lists 'x', 'y'
+            DataOutput = pd.DataFrame(self.wpdict)
+            DataOutput.to_csv("./ros_wall_follower/scripts/coords_mamado_ultimo.csv" , index=False)
 
 if __name__ == "__main__":
-    rospy.init_node("dummy_agent")
     robot = WallFollower()
     # robot.main()
     rospy.spin()
