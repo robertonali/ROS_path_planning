8ab78f19541f900564d5e73ae36f8c4a2ec939b3
Fernando Zaragoza <55164446+fezaragoza@users.noreply.github.com>
2020-09-02 18:31:06 -0500

    Merge dev week2 python work. (#7)
    * Inicio semana 2
    
    * creación lateral kinematics
    
    * Added lat kinematics params calculation
    
    * Lateral Kinematics SIN control
    
    * Cambios wallfollower para pure pursuit
    
    * Cleanup python file in a new one.
    
    * wall follower 300ms, lateral kinematics next wp change from 0.05 to .15
    
    * row 61 deleted
    
    * Cleanup file ros_wf.py with pandas. Working.
    
    * Added quaternions and euler.
    Changes in getWaypoints to save points related to the front of the car.
    Change pandas to numpy.
    
    * Moved code from ros_wf.py to ros_wall_follower.
    
    * ros_wf.py (now deleted)
    
    * Calculated ld(lookahead distance) instead of geometric ld
    
    * pure pursuit sin PID final lookahead distance mayor a los puntos para ver los puntos siguientes al siguiente mas proximo, wall follower ajuste de distancia de guardar puntos de 1 vez el wheelbase a .1 veces y limitar la velocidad segun salida de pid
    
    * SLAM maps/hector slam and lateral kinematics final
    
    * Moved slam files around, added gmapping launch file and results.
    
    Removing gmapping bag file
    
    Bag files removed from git.
    
    * Moved csv files to proper folder inside scripts.
    
    * ROS Wall Follower py file cleanup.
    
    * Cleanup of PurePursit LateralKin Control.
    
    * Changes to lateral control, include PD controller. Adjusted gains.
    
    * Changing running calcLateraKinematics each timerCallback. Working, with
    or without PID, same gains
    
    * CLEANUP Lateral Kinem.
    
    Co-authored-by: DESKTOP-D1FOK76\Chets <TheKillercheto@gmail.com>

---
+++ scripts/dummy_agent_node4.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+# all the used libraries are imported
+import rospy
+from ackermann_msgs.msg import AckermannDriveStamped
+from sensor_msgs.msg import LaserScan
+import numpy as np
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+# The class is defined 
+class Agent(object):
+    def __init__(self):
+        
+        # wall follower variables
+        self.r = 0
+        self.l = 0
+        self.error = 0
+        self.error_1 = 0
+        self.dt = 0.01
+
+        self.pid = 0
+
+        # odometry variables are created
+        self.current_x = 0
+        self.current_y = 0
+        self.current_v_x = 0
+        self.current_v_y = 0
+        self.current_v = (self.current_v_x**2 + self.current_v_y**2) ** 0.5
+        self.current_roll = 0
+        self.current_pitch = 0
+        self.current_yaw = 0
+
+        # publishers and subscribers
+        self.odom_list = list()
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback, queue_size=1)
+        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback, queue_size=1)
+        self.drive_pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=1)
+        rospy.Timer(rospy.Duration(self.dt), self.timer_callback)
+
+    # this function is accessed every 0.01 seconds
+    def timer_callback(self, elapsed):
+        self.error_1 = self.error 
+
+        # the difference between the left and right measurements is computed
+        self.error = self.l - self.r
+
+        # pid variables
+        p = 0.24 * self.error
+        d = 0.12 * (self.error - self.error_1) / self.dt
+        self.pid = p + d
+
+        # linearization of the velocity according to the pid
+        v = 5.0 - abs(self.pid) * 3.0/1.22
+
+        # speed and steering angle are published
+        drive = AckermannDriveStamped()
+        drive.drive.speed = v
+        drive.drive.steering_angle = self.pid
+        self.drive_pub.publish(drive)
+        rospy.loginfo("vel:{}".format(v))
+
+    def scan_callback(self, scan_msg):
+        # left and right measurements are aquired
+        r_segment = np.array(scan_msg.ranges[140:540])
+        l_segment = np.array(scan_msg.ranges[-540:-140])
+        
+        # a mean is computed for each side
+        self.r = np.mean(r_segment)
+        self.l = np.mean(l_segment)
+
+    def odom_callback(self, odom_msg):
+        # orientation data is acquired - data is transformed from quat to euler matrix
+        orientation_q = odom_msg.pose.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        self.yaw_1 = self.current_yaw
+        (self.current_roll, self.current_pitch, self.current_yaw) = euler_from_quaternion (orientation_list)
+
+        # linear odometry is acquired
+        self.current_x = odom_msg.pose.pose.position.x + 0.17*np.cos(self.current_yaw)
+        self.current_y = odom_msg.pose.pose.position.y + 0.17*np.sin(self.current_yaw)
+        self.current_v_x = odom_msg.twist.twist.linear.x
+        self.current_v_y = odom_msg.twist.twist.linear.y
+        self.current_v = (self.current_v_x**2 + self.current_v_y**2) ** 0.5
+        self.current_v_th = np.arctan2(self.current_v_y, self.current_v_x)
+
+        # measurements are stored in a dat file
+        self.odom_list.append([self.current_x, self.current_y, self.current_v])
+        np.savetxt('./ros_wall_follower/scripts/odom_dummy_data.csv', self.odom_list, delimiter=',')
+
+if __name__ == '__main__':
+    rospy.init_node('dummy_agent')
+    dummy_agent = Agent()
+    rospy.spin()
---
+++ scripts/odom_node.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python
+
+# import ros stuff
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist, Point
+from nav_msgs.msg import Odometry
+from tf import transformations
+
+import math
+
+# robot state variables
+position_ = Point()
+yaw_ = 0
+# machine state
+state_ = 0
+# goal
+desired_position_ = Point()
+desired_position_.x = -3
+desired_position_.y = 7
+desired_position_.z = 0
+# parameters
+yaw_precision_ = math.pi / 90 # +/- 2 degree allowed
+dist_precision_ = 0.3
+
+# publishers
+pub = None
+def clbk_odom(msg):
+    global position_
+    global yaw_
+    
+    # position
+    position_ = msg.pose.pose.position
+    
+    # yaw
+    quaternion = (
+        msg.pose.pose.orientation.x,
+        msg.pose.pose.orientation.y,
+        msg.pose.pose.orientation.z,
+        msg.pose.pose.orientation.w)
+    euler = transformations.euler_from_quaternion(quaternion)
+    yaw_ = euler[2]
+
+
+def change_state(state):
+    global state_
+    state_ = state
+    print 'State changed to [%s]' % state_
+
+def fix_yaw(des_pos):
+    global yaw_, pub, yaw_precision_, state_
+    desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
+    err_yaw = desired_yaw - yaw_
+    
+    twist_msg = Twist()
+    if math.fabs(err_yaw) > yaw_precision_:
+        twist_msg.angular.z = -0.3 if err_yaw > 0 else 0.3
+    
+    pub.publish(twist_msg)
+    
+    # state change conditions
+    if math.fabs(err_yaw) <= yaw_precision_:
+        print 'Yaw error: [%s]' % err_yaw
+        change_state(1)
+
+def go_straight_ahead(des_pos):
+    global yaw_, pub, yaw_precision_, state_
+    desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
+    err_yaw = desired_yaw - yaw_
+    err_pos = math.sqrt(pow(des_pos.y - position_.y, 2) + pow(des_pos.x - position_.x, 2))
+    
+    if err_pos > dist_precision_:
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.3
+        pub.publish(twist_msg)
+    else:
+        print 'Position error: [%s]' % err_pos
+        change_state(2)
+    
+    # state change conditions
+    if math.fabs(err_yaw) > yaw_precision_:
+        print 'Yaw error: [%s]' % err_yaw
+        change_state(0)
+
+def main():
+    global pub
+    
+    rospy.init_node('go_to_point')
+    
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    sub_odom = rospy.Subscriber('/odom', Odometry, clbk_odom)
+    
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        if state_ == 0:
+            fix_yaw(desired_position_)
+        elif state_ == 1:
+            go_straight_ahead(desired_position_)
+        elif state_ == 2:
+            done()
+            pass
+        else:
+            rospy.logerr('Unknown state!')
+            pass
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
